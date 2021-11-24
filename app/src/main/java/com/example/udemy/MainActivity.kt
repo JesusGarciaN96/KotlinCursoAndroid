4c47ff3083631fa55e77cf5d3383d1b76186ad9f
Jesus Alejandro Garcia <garciacode96@gmail.com>
2021-11-24 16:33:21 -0600

    Curso de Kotlin

---
+++ app/src/main/java/com/example/udemy/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.udemy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    mostrarMensajeSaludo("Hola, bienvenido a android!")
+    variablesConstantes()
+  }
+
+  private fun mostrarMensajeSaludo(mensajeSaludo: CharSequence){
+    var textoSaludo = findViewById<TextView>(R.id.main_id_text);
+    var botonSaludo = findViewById<Button>(R.id.main_id_button);
+    botonSaludo.setOnClickListener{
+      textoSaludo.text = if(textoSaludo.text == "Mensaje Fake!") mensajeSaludo else "Mensaje Fake!";
+    }
+  }
+
+  // Curso de Kotlin para principiantes
+  private fun variablesConstantes(){
+    
+  }
+}
